2f2292b17755f4ef85720f3dc675cbb3aa324315
Lee DongJun <ejdejdwns@gmail.com>
2022-06-11 12:06:55 +0900

    vocab+modeling

--- model.py
+++ model.py
@@ -1,6 +1,71 @@
 import torch
 import torch.nn as nn
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+torch.cuda.set_device(device) # change allocation of current GPU
+#print ('# Current cuda device: ', torch.cuda.current_device())
+HIDDEN_SIZE = 100
+OUTPUT_CLASS = 4
+EMBED_SIZE = 128
+class Encoder(nn.Module):
+    def __init__(self,vocab_size):
+        super().__init__()
+        self.embed = nn.Embedding(vocab_size, EMBED_SIZE).to(device = device)#word_sizexembed size matrix. 정수->벡터 함수
+        self.LSTM = nn.LSTM(input_size = EMBED_SIZE , hidden_size= HIDDEN_SIZE, batch_first=True).to(device = device)
+ 
+    def forward(self, x):
+        x = x#x shape: batch*sentence_size
+        x = self.embed(x).to(device = device)#x.shape = batchxsentence size(문장의 토큰 개수)x embed_size
+        output, final_status = self.LSTM(x)
+        return output, final_status# shape: batch x sentence_size(LSTM이 돈 횟수) x hidden_size_sx
 
+class Attention_LSTM(nn.Module):
+    def __init__(self,vocab_size):
+        super().__init__()
+        self.encoder=Encoder(vocab_size)
+        self.decode_embed = nn.parameter(torch.randn(1,HIDDEN_SIZE).to(device=device), requires_grad = True)
+        self.hiddenstate = nn.Sequential(nn.Linear(HIDDEN_SIZE*2, HIDDEN_SIZE), nn.BatchNorm1d(HIDDEN_SIZE), nn.Linear(HIDDEN_SIZE, OUTPUT_CLASS)).to(device = device)
+        
+    def forward(self, x):#x: 숫자 리스트
+        encoder_result, final_encoder_status = self.encoder(x)#batch*sentence size*embedding    
+        for iteration in range(1, en_sentence_size):
+            attention_score = torch.matmul(encoder_result,self.decode_embed.transpose(1,2))#decoder_embed를 column vector로 바꿔주었다. final shape = batch*encoder sentence size* 1(디코더에는 한 번에 한 token만 넣어주므로..)
+            #
+            attention_distribution = softmax(attention_score, dim=1)
+            #print("ALSTM attention_distribution:", attention_distribution)#debug
+            attention_value = torch.sum(encoder_result * attention_distribution, dim=1).unsqueeze(1)#아다마르 곱 후 unsqueeze 로 batch*1*hidden_size 로 바꿔준다
+            #print("ALSTM attention_value:", attention_value)#debug
+            att_dec_concat = torch.cat((attention_value, decoder_result), dim = 2).squeeze(1)#batch*hidden_sie*2
+            #print("ALSTM input for final Linear layer:", att_dec_concat)#debug
+            final_vector = torch.tanh(self.hiddenstate(att_dec_concat))#batch*hidden_s
+            a_output = self.outputLayer(final_vector)#batch*en_word_size
+            #print("ALSTM output of final Linear layer:", output)#debug
+            #output_softmax = softmax(output, dim=1)
+            #output_lsm = logsoftmax(output,dim=1)
+            #print("ALSTM Logsoftmax:", output_lsm)#debug
+            
+            a_output_argmax = a_output.argmax(dim=1).view(-1,1)
+            current_truth = ground_truth[:,iteration]
+            
+            #print(iteration, output_softmax[:,current_truth].shape)
+            #print(current_truth)
+
+            if self.training == True:#what to do in training mode -> model.train()...
+                if  tf == 1:
+                    en_generated = torch.cat((en_generated, current_truth.unsqueeze(1)), dim=1)#teacher forcing
+                else:
+                    en_generated = torch.cat((en_generated, a_output_argmax), dim=1)#Non_teacher_forcing
+            else:
+                en_generated = torch.cat((en_generated, a_output_argmax), dim=1)
+            #prob_truth = output_lsm[[idx for idx in range(output_lsm.shape[0])],current_truth].unsqueeze(1)
+            if output is None:#shape = batch*sentence_size2
+                output = a_output.unsqueeze(1)#batch*1*en 사전크기
+                #print(output.shape)
+            else:
+                output = torch.cat((output, a_output.unsqueeze(1)),dim=1)
+                #print(output.shape)
+            #print(en_generated)
+        #print("tensor size of output: ", gpusize(output))# debug
+        return en_generated, output
 
 class BaseModel(nn.Module):
     '''
--- vocab.py
+++ vocab.py
@@ -1,60 +1,183 @@
 from collections import Counter
+import re, collections
+from tqdm.auto import tqdm
+from os.path import exists 
+import pickle
+ENDWORD_TOKEN = '</w>'
+
+def get_stats(dictionary):#dic: whole dic. 문서의 모든 단어 전체
+    # 유니그램의 pair들의 빈도수를 카운트
+    pairs = collections.defaultdict(int)
+    for word, freq in dictionary.items():
+        symbols = word.split()#whitespace를 기준으로 나눈다. 때문에 사전에 분리가 되어있어야 함
+        for i in range(len(symbols)-1):
+            pairs[symbols[i],symbols[i+1]] += freq #tuple 이 key가 된다.
+    return pairs
+
+def merge_dictionary(pair, v_in):
+    v_out = {}
+    bigram = re.escape(' '.join(pair))# 묶고싶은 unigram은 공백으로 떨어져 있을 것이므로...
+    p = re.compile(r'(?<!\S)' + bigram + r'(?!\S)')
+    for word in v_in:
+        w_out = p.sub(''.join(pair), word)#word 안에서 pattern에 맞는 부분을 join 해서 교체한다
+        v_out[w_out] = v_in[word]#그걸 다시 key로 등록
+    return v_out
+
+
+
+
+
+
+def get_pairs(word):
+    """Return set of symbol pairs in a word.
+    Word is represented as a tuple of symbols (symbols being variable-length strings).
+    """
+    pairs = set()
+    prev_char = word[0]
+    for char in word[1:]:
+        pairs.add((prev_char, char))
+        prev_char = char
+    return pairs
+
+
+
 
 class Vocabulary:
-
-    def __init__(self, vocab_size):
-        """
-        TODO: Change vocabulary code as you need. (e.g. tokenizer, using stopword, etc.)
-        
-        vocab_size : max source vocab size. Eg. if set to 10,000, we pick the top 10,000 most frequent words and discard others
-        """
+    #byte-pair encoded vocab
+    def __init__(self, document, num_merges = 10000):
 
         ## <PAD> -> padding, used for padding the shorter sentences in a batch to match the length of longest sentence in the batch
         ## <UNK> -> words which are not found in the vocab are replace by this token
-        self.vocab_size = vocab_size
-        self.itow = {0: '<PAD>',1: '<UNK>'}
-        self.wtoi = {w:i for i, w in self.itow.items()}
-
+        if exists('./vocab.pickle'):
+            with open('./vocab.pickle','rb') as file:
+                data = pickle.load(file)
+            self.num_merges, self.itow, self.wtoi, self. wtoi_reverse = data
+        else:
+            self.num_merges = num_merges
+            self.itow = {0: '<PAD>',1: '<UNK>',2: ENDWORD_TOKEN,}
+            self.wtoi = {w:i for i, w in self.itow.items()}
+            self.wtoi_reverse = {'<PAD>':'<PAD>', '<UNK>':'<UNK>', ENDWORD_TOKEN:ENDWORD_TOKEN,}
+            self.build_vocabulary(document)
+            wtoi_r_r = {t:w for w, t in self.wtoi_reverse.items()}
+            self.itow = {i:wtoi_r_r[w] for w, i in self.wtoi.items()}
+            with open('./vocab.pickle','wb') as file:
+                    data = pickle.dump((self.num_merges, self.itow, self.wtoi, self. wtoi_reverse),file)
 
     def __len__(self):
         return len(self.itow)
 
-    def tokenizer(self, text):
-        return [tok.lower().strip() for tok in text.split(' ')]
+    def spacer(self, text):
+        return [tok.lower().strip() for tok in text.split(' ')]#무조건 lowercase로만 작동
 
-    def build_vocabulary(self, sentences, add_unk=True):
+    def build_vocabulary(self, sentences,):#sentence: pandas로 불러온뒤 text 줄만...
+        to_wtoi, to_wtoi_r = self.count_characters(sentences)
+        self.wtoi.update(to_wtoi)
+        self.wtoi_reverse.update(to_wtoi_r)
+        #여기까지 문서 안의 모든 문자 추가 완료
         word_list = []
-        idx=2 #index from which we want our dict to start. We already used 2 indexes for pad and unk
-
-        if add_unk == False: # build vocab for label
-            self.wtoi = {}
-            idx=0
-        
         for sentence in sentences:
-            for word in self.tokenizer(sentence):
+            for word in self.spacer(sentence):
                 if word:
-                    word_list.append(word)
+                    word_list.append(' '.join(tuple(word))+ENDWORD_TOKEN)
                     
-        freq = Counter(word_list)
-        freq_ = sorted(freq,key=freq.get,reverse=True)
+        document_freq = dict(Counter(word_list))
+        self.generate_bpe_vocab(document_freq,self.num_merges)
 
-        for word in freq_[:self.vocab_size-idx]:
-            self.wtoi[word] = idx
-            self.itow[idx] = word
-            idx += 1
-        
+    def generate_bpe_vocab(self,dictionary, num_merges):
+        value_max = max(self.wtoi.values())
+        for i in tqdm(range(num_merges)):
+            pairs = get_stats(dictionary)
+            best = max(pairs, key=pairs.get)#문서 전체에서 가장 빈도수가 높은 pair
+            dictionary = merge_dictionary(best, dictionary)
 
-    def sentence_to_numeric(self, text):
-
-        tokenized_text = self.tokenizer(text)
-        numeric_text = []
-        for token in tokenized_text:
-            if token in self.wtoi:
-                numeric_text.append(self.wtoi[token])
-            else: # out-of-vocab words are replaced by <UNK>
-                numeric_text.append(self.wtoi['<UNK>'])
-
-        return numeric_text
+            self.wtoi[best] = i+value_max+1
+            self.wtoi_reverse[best[0] + best[1]] = best
 
 
-            
+    def count_characters(self, sentences):
+        value_max = max(self.wtoi.values())
+        sentence_list = []
+        for sentence in tqdm(sentences):
+            sentence_list.append(sentence.lower())
+        merged = list(tuple(''.join(sentence_list)))
+        merged = sorted(list(set(merged)))
+        merged_dict = {c:i for c,i in zip(merged,range(value_max+1,value_max+len(merged)+1))}
+        merged_dict_reverse = {i:i for i,v in merged_dict.items()}
+        return merged_dict, merged_dict_reverse
+
+    def encode(self, orig):#bpe에 의거해서 주어진 단일 word를 잘라서 출력
+        """Encode word based on list of BPE merge operations, which are applied consecutively"""
+
+        word = tuple(orig) + (ENDWORD_TOKEN,)#단어의 끝에 존재하는 unigram인 경우를 체크하려는듯
+        #display(Markdown("__word split into characters:__ <tt>{}</tt>".format(word)))
+
+        pairs = get_pairs(word)
+
+        if not pairs:
+            return orig
+
+        iteration = 0
+        while True:
+            iteration += 1
+            #display(Markdown("__Iteration {}:__".format(iteration)))
+
+            bigram = min(pairs, key = lambda pair: self.wtoi.get(pair, float('inf')))
+            if bigram not in self.wtoi:
+                #display(Markdown("__Candidate not in BPE merges, algorithm stops.__"))
+                break
+            first, second = bigram
+            new_word = []
+            i = 0
+            while i < len(word):
+                try:
+                    j = word.index(first, i)
+                    new_word.extend(word[i:j])
+                    i = j
+                except:
+                    new_word.extend(word[i:])
+                    break
+
+                if word[i] == first and i < len(word)-1 and word[i+1] == second:
+                    new_word.append(first+second)
+                    i += 2
+                else:
+                    new_word.append(word[i])
+                    i += 1
+            new_word = tuple(new_word)
+            word = new_word
+            if len(word) == 1:
+                break
+            else:
+                pairs = get_pairs(word)
+        """
+        # 특별 토큰인 </w>는 출력하지 않는다.
+        if word[-1] == ENDWORD_TOKEN:
+            word = word[:-1]
+
+        elif word[-1].endswith(ENDWORD_TOKEN):#Unigram 자체가 /w를 포함하는 경우.
+            word = word[:-1] + (word[-1].replace(ENDWORD_TOKEN,''),)
+        """
+        def return_index(string):
+            try:
+                return self.wtoi[self.wtoi_reverse[string]]
+            except KeyError:
+                return 1#존재하지 않는 단어
+        return list(map(return_index,word))
+
+    def __call__(self, string):
+        tokenized = []
+        spaced = self.spacer(string)
+        for word in spaced:
+            tokenized = tokenized + self.encode(word)
+        return tokenized
+    def itow_(self, tokenlist):
+        return ''.join(list(map(lambda x: self.itow[x], tokenlist)))
+import pandas as pd
+df = pd.read_csv('./Data/train.csv')
+test_doc = df['text'].values.tolist()
+test_vocab = Vocabulary(test_doc,1000)
+for i in range(100):
+    sentences = test_doc[i]
+    tokenized = test_vocab(sentences)
+    print(tokenized)
+    print(test_vocab.itow_(tokenized))
